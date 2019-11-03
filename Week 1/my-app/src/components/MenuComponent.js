b1994b31555c8f68541e70121114447e5ddfd147
vasilijus <vasiliokas@gmail.com>
2019-11-03 21:23:42 +0000

    working select Item , and display Item + details in a new Card

--- Week 1/my-app/src/components/MenuComponent.js
+++ Week 1/my-app/src/components/MenuComponent.js
@@ -16,6 +16,25 @@ class Menu extends Component {
         this.setState({ selectedDish: dish })
     }
 
+    renderDish(dish) {
+        if (dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        }
+        else {
+            return(
+                <div></div>
+            );
+        }
+    }
+
     render() {
         const menu = this.props.dishes.map(dish => {
             return (
@@ -34,9 +53,10 @@ class Menu extends Component {
         return (
             <div className="container">
                 <div className="row">
-
                         {menu}
-
+                </div>
+                <div className="row">
+                        {this.renderDish(this.state.selectedDish)}
                 </div>
             </div>
         );
