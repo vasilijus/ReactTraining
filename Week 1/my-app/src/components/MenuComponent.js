b4a4c36ba1839c45e0ecb400d8417065a92819ea
vasilijus <vasiliokas@gmail.com>
2019-11-03 21:16:53 +0000

    Added a function selected dish,  with onclick event

--- Week 1/my-app/src/components/MenuComponent.js
+++ Week 1/my-app/src/components/MenuComponent.js
@@ -8,15 +8,19 @@ class Menu extends Component {
         super(props);
 
         this.state = {
-            
+            selectedDish: null,
         }
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish })
     }
 
     render() {
         const menu = this.props.dishes.map(dish => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card>
+                    <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                      
                         <CardImgOverlay>
