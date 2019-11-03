9ba8387b89430071dfea41e58e75373b0dbf3d81
vasilijus <vasiliokas@gmail.com>
2019-11-03 01:09:20 +0000

    bootstap, reactStrap Navbar added

--- Week 1/my-app/src/App.js
+++ Week 1/my-app/src/App.js
@@ -1,12 +1,18 @@
 import React,{ Component } from 'react';
 import logo from './logo.svg';
+import { Navbar } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <Navbar dark color="primary">
+          <div className="container">
+            
+          </div>
+        </Navbar>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +25,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
